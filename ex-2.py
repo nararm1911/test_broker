29594b96b63ee0e79ae48d7e410650a7644976ad
narek <70536368+nararm1911@users.noreply.github.com>
2022-06-01 21:36:24 +0400

    version 1

---
+++ ex-2.py
@@ -0,0 +1,13 @@
+
+#ex-2
+
+a = input("a" '=')
+b = input("b" '=')
+f = input("f" '=')
+x = (int(a)+int(b)-int(f)/int(a))+int(f)*int(a)*int(a)-(int(a)+int(b))
+
+
+print(x)
+
+
+#(a + b - f / a) + f * a * a - (a + b)
